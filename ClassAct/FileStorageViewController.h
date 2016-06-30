b9a36e6598f657f44d0e2e9e8852bea0b3c53db8
Jeremy Lilje <jeremylilje@Jeremys-MacBook-Pro.local>
2016-06-30 10:44:16 -0400

    part of the storage updates... not yet complete

---
+++ ClassAct/FileStorageViewController.h
@@ -0,0 +1,13 @@
+//
+//  FileStorageViewController.h
+//  ClassAct
+//
+//  Created by Jeremy Lilje on 6/29/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FileStorageViewController : UIViewController
+
+@end

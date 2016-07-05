61dbd272ef6f2936dd41cdd531278fdd9cc5516e
Jeremy Lilje <jeremylilje@Jeremys-MacBook-Pro.local>
2016-07-05 13:56:40 -0400

    firebase storage works for jeremy only

--- ClassAct/FileStorageViewController.m
+++ ClassAct/FileStorageViewController.m
@@ -11,8 +11,7 @@
 #import <Firebase.h>
 #import <UIKit/UIKit.h>
 
-FIRStorageReference *docDir;
-FIRStorageReference *imgDir;
+FIRStorageReference *storageFileDir;
 
 @interface FileStorageViewController ()
 
@@ -29,12 +28,10 @@ FIRStorageReference *imgDir;
         [self.sideBarButton setAction: @selector( revealToggle: )];
         [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
     }
-//    FIRStorage *storage = [FIRStorage storage];
     // Do any additional setup after loading the view.
     FIRStorage *storage = [FIRStorage storage];
     FIRStorageReference *storageRef = [storage referenceForURL:@"gs://classact-22396.appspot.com"];
-    docDir = [storageRef child:@"documents"];
-    imgDir = [storageRef child:@"images"];
+    storageFileDir = [storageRef child:@"files"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -46,16 +43,44 @@ FIRStorageReference *imgDir;
     NSLog(@"********************************************************");
     NSLog(@"========================================================");
     NSLog(@"upload button pushed");
-    FIRStorageReference *testDocRef = [docDir child:@"testDocument.txt"];
-    FIRStorageReference *testImgRef = [imgDir child:@"testImage.jpg"];
-    NSLog(@"test document reference is %@", testDocRef);
-    NSLog(@"test image reference is %@", testImgRef);
+    
+    NSString *localSourceForUploadFileStringDir = @"/Users/jeremylilje/DetroitLabsProjects/week07/ClassAct/ClassAct/";
+    NSString *localSourceForUploadFileStringFile = _UploadFileTextField.text;
+    NSString *localSourceForUploadFileString = [localSourceForUploadFileStringDir stringByAppendingString:localSourceForUploadFileStringFile];
+    NSURL *localSourceForUploadFileRef = [NSURL fileURLWithPath:localSourceForUploadFileString];
+    NSLog(@"localSourceForUploadFileRef is %@", localSourceForUploadFileRef);
+    
+    NSString *remoteTargetForUploadFileString = _UploadFileTextField.text;
+    FIRStorageReference *remoteTargetForUploadFileRef = [storageFileDir child:remoteTargetForUploadFileString];
+    NSLog(@"remoteTargetForUploadFileRef is %@", remoteTargetForUploadFileRef);
+
+    FIRStorageUploadTask *uploadTask = [remoteTargetForUploadFileRef putFile:localSourceForUploadFileRef];
 }
 
 - (IBAction)DownloadButton:(id)sender {
     NSLog(@"********************************************************");
     NSLog(@"========================================================");
     NSLog(@"download button pushed");
+    
+    NSString *remoteSourceForDownloadFileString = _DownloadFileTextField.text;
+    FIRStorageReference *remoteSourceForDownloadFileRef = [storageFileDir child:remoteSourceForDownloadFileString];
+    NSLog(@"remoteSourceForDownloadFileRef is %@", remoteSourceForDownloadFileRef);
+    NSString *localTargetForDownloadFileStringDir = @"/Users/jeremylilje/DetroitLabsProjects/week07/ClassAct/ClassAct/";
+    NSString *localTargetForDownloadFileStringFile = _DownloadFileTextField.text;
+    NSString *localTargetForDownloadFileString = [localTargetForDownloadFileStringDir stringByAppendingString:localTargetForDownloadFileStringFile];
+    NSURL *localTargetForDownloadFileRef = [NSURL fileURLWithPath:localTargetForDownloadFileString];
+    NSLog(@"localTargetForDownloadFileRef is %@", localTargetForDownloadFileRef);
+    
+    FIRStorageDownloadTask *downloadTask = [remoteSourceForDownloadFileRef writeToFile:localTargetForDownloadFileRef completion:^(NSURL * _Nullable URL, NSError * _Nullable error)
+    {
+        if (error != nil)
+        {
+            NSLog(@"an error occurred %@", error);// Uh-oh, an error occurred!
+        } else
+        {
+            NSLog(@"file downloaded successfully!");// Local file URL for "images/island.jpg" is returned
+        }
+    }];
 }
 
 /*
